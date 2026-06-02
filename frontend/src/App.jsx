2aea800d023c021b7716c132247d246309a44a6b
KRISH-PROJECT <saikrishna5889382@gmail.com>
2026-06-02 17:55:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Find Nearby Electricians</h1>
+
+      <button>Book Service</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>FixLocal</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
